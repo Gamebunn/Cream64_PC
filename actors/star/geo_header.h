42744eb8a9c8568b8df93c419603ccac0019af38
Gamebunn <gerardobarrer@gmail.com>
2023-02-25 14:03:12 -0600

    Updated star models

--- actors/star/geo_header.h
+++ actors/star/geo_header.h
@@ -1,7 +1,7 @@
 extern const GeoLayout star_geo[];
 extern u8 star_cmn_obj_starring_ref_SD_32bit_ci8[];
 extern u8 star_cmn_obj_starring_ref_SD_32bit_ci8_pal_rgba16[];
-extern Vtx star_000_displaylist_mesh_layer_1_vtx_0[201];
+extern Vtx star_000_displaylist_mesh_layer_1_vtx_0[311];
 extern Gfx star_000_displaylist_mesh_layer_1_tri_0[];
 extern Gfx mat_star_star_texture[];
 extern Gfx mat_revert_star_star_texture[];
